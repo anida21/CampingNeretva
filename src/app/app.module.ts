9ab83a956cc18ebac28988528908a3e38c38d4aa
ENDAVA\amujezin <anida.mujezin@endava.com>
2024-02-23 12:17:36 +0100

    Izmjene fonta, i odredjenih stilova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
