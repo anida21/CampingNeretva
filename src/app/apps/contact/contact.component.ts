67939eb38afc3dd5bd2e87bb0361b159501aab07
ENDAVA\amujezin <anida.mujezin@endava.com>
2024-03-10 19:25:21 +0100

    Contact component

--- src/app/apps/contact/contact.component.ts
+++ src/app/apps/contact/contact.component.ts
@@ -1,14 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmailForm } from 'src/app/reservation/email.model';
+import { EmailService } from 'src/app/reservation/email.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() { }
+  emailForm: EmailForm = {
+    to: 'didamujezin@gmail.com',
+    formData: {
+      Name: '',
+      PhoneNumber: '',
+      Email: '',
+      TypeOfVehicle: '',
+      CheckBox1: false,
+      CheckBox2: false,
+      NumberOfGuests: '',
+      CheckIn: '',
+      CheckOut: '',
+      Message: '',
+      TypeOfCamping: ''
+    }
+  };
 
-  ngOnInit(): void {
+  
+constructor(private emailService: EmailService,
+  private router: Router) {}
+
+  sendMessage() {
+    this.emailService.sendEmail(this.emailForm).subscribe(
+      response => {
+        console.log('Email sent successfully', response);
+        // Nakon uspješnog slanja emaila, preusmjeri korisnika na drugu stranicu
+        this.router.navigateByUrl('/successful-reservation');
+      },
+      error => {
+        console.error('Error sending email:', error);
+      }
+    );
   }
+  
+
 }
