554738bb03f94c3876008404ab29ec87b71e8831
ENDAVA\amujezin <anida.mujezin@endava.com>
2024-03-17 13:52:44 +0100

    Meta tags, and compresed images

--- src/app/apps/contact/contact.component.ts
+++ src/app/apps/contact/contact.component.ts
@@ -2,13 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmailForm } from 'src/app/reservation/email.model';
 import { EmailService } from 'src/app/reservation/email.service';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+    
+constructor(private emailService: EmailService,
+  private router: Router,
+  private meta: Meta) {}
+
+
+  ngOnInit(): void {
+    this.meta.addTags([
+      { name: 'description', content: 'Description of the contact page' },
+      { name: 'keywords', content: 'contact, camping neretva, location, phone, email' },
+      { property: 'og:title', content: 'Contact | Camping Neretva' },
+      { property: 'og:description', content: 'Contact information for Camping Neretva' },
+      { property: 'og:image', content: 'assets/images/innerpage/naturee.jpg' },
+      { property: 'og:url', content: 'https://www.campingneretva.com/contact' },
+      { name: 'twitter:title', content: 'Contact | Camping Neretva' },
+      { name: 'twitter:description', content: 'Contact information for Camping Neretva' },
+      { name: 'twitter:image', content: 'assets/images/innerpage/naturee.jpg' },
+      { name: 'twitter:card', content: 'summary_large_image' }
+    ]);
+  }
+
 
   emailForm: EmailForm = {
     to: 'didamujezin@gmail.com',
@@ -27,9 +50,6 @@ export class ContactComponent {
     }
   };
 
-  
-constructor(private emailService: EmailService,
-  private router: Router) {}
 
   sendMessage() {
     this.emailService.sendMessage(this.emailForm).subscribe(
@@ -47,6 +67,5 @@ constructor(private emailService: EmailService,
   isSubmitDisabled(): boolean {
     return (this.emailForm.formData.Name === '' || this.emailForm.formData.Email === '');
   }
-
 
 }
